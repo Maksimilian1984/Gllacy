cba5fe173025bf0e8738c7fbeef46e3779aa91a8
Maksimilian1984 <58360374+Maksimilian1984@users.noreply.github.com>
2020-06-25 00:45:58 +0300

    переписал js для popup

--- slider.js
+++ slider.js
@@ -8,10 +8,10 @@ const bodyTheme = document.querySelector('body');
 const radioSlider1 = document.querySelector('.radio-slider-1');
 const radioSlider2 = document.querySelector('.radio-slider-2');
 const radioSlider3 = document.querySelector('.radio-slider-3');
-const modalFeedback = document.querySelector('.modal-feedback');
+
 const buttonContacts = document.querySelector('.button-contacts');
-const closeModalFeedback = document.querySelector('.close-modal-feedback');
-const overlayWindow = document.querySelector('.overlay-window');
+
+
 
 
 const checkInput =  (button, slider1, slider2, color) => {
@@ -40,38 +40,136 @@ const runSlider = () => {
 
 
 
-
-
-
 setInterval(runSlider, 4000);
 
 
 
-// для модального окна
-
-const onEscDown = (evt) => {
-    const isEscape = evt.key  === 'Esc' || evt.key === 'Escape';
-    isEscape ? onClosePopup() : '';
 
 
-};
 
-const onPopup = () => {
-    modalFeedback.classList.add('visible');
-    overlayWindow.classList.add('visible');
-    document.addEventListener('keydown', onEscDown)
-};
-
-const onClosePopup = () => {
-    modalFeedback.classList.remove('visible');
-    overlayWindow.classList.remove('visible');
-    document.removeEventListener('keydown' , onEscDown)
+const createTemplatePopup = () => {
+    return (`<div class="modal-feedback">
+        <p>Мы обязательно вам ответим!</p>
+        <button class="close-modal-feedback"></button>
+        <form action="index.html">
+            <input class="modal-feedback-name" type="text" placeholder="Как вас зовут?">
+            <input class="modal-feedback-email" type="email" name="email" id="email" placeholder="Ваша почта для ответа?">
+            <textarea class="modal-feedback-message" name="modal-feedback-message" id="modal-feedback-message" cols="50" rows="8" placeholder="Напишите что-нибудь..."></textarea>
+            <button class="button sent-feedback-form">Отправить</button>
+        </form>
+    </div>
+   `)
 };
 
 
-buttonContacts.addEventListener('click', onPopup);
 
-closeModalFeedback.addEventListener('click', onClosePopup);
+const createTemplateOverlay = () => {
+    return (`<div class="overlay-window"></div>`)
+};
+
+
+
+const createElement = (template) => {
+    const newElement = document.createElement('div');
+
+    newElement.innerHTML = template;
+    return newElement.firstChild
+
+};
+
+
+
+class Popup  {
+    constructor() {
+        this._element = null;
+    }
+
+    getTemplait(){
+        return createTemplatePopup();
+
+    }
+
+    getElement(){
+        if(!this._element) {
+            this._element = createElement(this.getTemplait());
+
+        };
+        return this._element;
+    }
+
+    removeElement(){
+        this._element.remove();
+    }
+
+};
+
+
+
+class Overlay  {
+    constructor() {
+        this._element = null;
+    }
+
+    getTemplait(){
+        return createTemplateOverlay();
+
+    }
+
+    getElement(){
+        if(!this._element) {
+            this._element = createElement(this.getTemplait());
+
+        };
+
+        return this._element;
+
+    }
+
+    removeElement(){
+        this._element.remove();
+    }
+
+};
+
+
+const render = () => {
+    const PopupElenent = new Popup();
+    const OverlayElement = new Overlay();
+
+    const body = document.querySelector('body');
+
+    const onEscDown = (evt) => {
+        const isEscape = evt.key  === 'Esc' || evt.key === 'Escape';
+        isEscape ? onClosePopup() : '';
+    };
+
+    const onOpenPopup = () => {
+        body.append(PopupElenent.getElement());
+        body.append(OverlayElement.getElement());
+        document.addEventListener('keydown', onEscDown)
+    };
+
+    const onClosePopup = () => {
+        PopupElenent.removeElement();
+        OverlayElement.removeElement();
+        document.removeEventListener('keydown', onEscDown)
+
+    };
+
+
+    buttonContacts.addEventListener('click', onOpenPopup);
+
+    const modalButtonClose = PopupElenent.getElement().querySelector('.close-modal-feedback')
+
+    modalButtonClose.addEventListener('click', onClosePopup)
+
+};
+
+
+render();
+
+
+
 
 
 
